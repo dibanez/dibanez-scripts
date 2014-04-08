0588dcc2df7484480a241b8a977b1576ed4ddeef
David Ibañez <dibanez@gmail.com>
2014-04-08 12:47:18 +0200

    Order scripts in folders

---
+++ networks/net2domains.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+for i in {1..254}
+do
+  myhostname=`host 192.168.0.$i`
+    if [ "$?" -eq 0 ]; then
+        echo -ne $i
+	    echo -ne "\t"
+	        echo -ne "IN"
+		    echo -ne "\t"
+		        echo -ne "PTR"
+			    echo -ne "\t"
+			        echo `echo $myhostname | cut -d " " -f 5`
+				  fi
+				  done
